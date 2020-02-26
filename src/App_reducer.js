7262c1f492e748ec5ace89e19d78989846fc3e07
dongsubak <likeap@catholic.ac.kr>
2020-02-26 18:55:43 +0900

    App_reducer.js

--- src/App_reducer.js
+++ src/App_reducer.js
@@ -2,3 +2,17 @@ const BOARD_SAVE ='SAVE';
 const BOARD_REMOVE = 'REMOVE';
 const BOARD_READ = 'ONE';
 const BOARD_LIST = 'LIST';
+
+export const board_save;
+
+export const board_remove;
+
+export const board_read;
+
+export const board_list;
+
+const initialState = {};
+
+export default function board_reducer(state = initialState, action) {
+    let boards = state.boards
+}
